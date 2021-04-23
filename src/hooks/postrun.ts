ad8aa3e9ec690eb2e8711127ff720eeb52a50dad
jayree <jayree.git@icloud.com>
2021-04-23 18:01:50 +0200

    fix: plugin uninstall

--- src/hooks/postrun.ts
+++ src/hooks/postrun.ts
@@ -10,14 +10,16 @@ import * as Debug from 'debug';
 
 const debug = Debug('pluginUpdateNotifier:postRun');
 
-export const postrun: Hook<'postrun'> = async function () {
-  if (env.getBoolean('OCLIF_DISABLE_UPDATENOTIFIER')) {
-    debug('found: OCLIF_DISABLE_UPDATENOTIFIER=true');
-    return;
+export const postrun: Hook<'postrun'> = async function (options) {
+  if (!['plugins:install', 'plugins:uninstall'].includes(options.Command.id)) {
+    if (env.getBoolean('OCLIF_DISABLE_UPDATENOTIFIER')) {
+      debug('found: OCLIF_DISABLE_UPDATENOTIFIER=true');
+      return;
+    }
+    await this.config.runHook('updateNotifier', {
+      updateCheckInterval: 1000 * 60 * 60 * 24,
+      spawnOptions: { detached: true, stdio: 'ignore' },
+      defer: true,
+    });
   }
-  await this.config.runHook('updateNotifier', {
-    updateCheckInterval: 1000 * 60 * 60 * 24,
-    spawnOptions: { detached: true, stdio: 'ignore' },
-    defer: true,
-  });
 };

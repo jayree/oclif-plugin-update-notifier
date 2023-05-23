d93c2564371c551354a301ec17c3cf5a6657f37d
jayree <jayree.git@icloud.com>
2023-05-23 23:43:00 +0200

    fix: remove spinner action

--- src/hooks/updatenotifier.ts
+++ src/hooks/updatenotifier.ts
@@ -4,7 +4,7 @@
  * Licensed under the BSD 3-Clause license.
  * For full license text, see LICENSE.txt file in the repo root or https://opensource.org/licenses/BSD-3-Clause
  */
-import { Hook, ux, Config } from '@oclif/core';
+import { Hook, Config } from '@oclif/core';
 import chalk from 'chalk';
 import Debug from 'debug';
 import { multiUpdateNotifier, Options } from '../utils/multiUpdateNotifier.js';
@@ -16,7 +16,6 @@ type HookOptions = Options & { config: Config } & {
 
 export const updateNotifier = async function (this: Hook.Context, options: HookOptions): Promise<void> {
   const debug = Debug(`${this.config.bin}:oclif-plugin-update-notifier:hooks:updatenotifier`);
-  if (!options.spawnOptions.detached) ux.action.start('check for updates');
   if (debug.enabled) options.spawnOptions.stdio = 'inherit';
   const notifier = new multiUpdateNotifier(this.config, {
     updateCheckInterval: options.updateCheckInterval,
@@ -37,7 +36,6 @@ export const updateNotifier = async function (this: Hook.Context, options: HookO
       notifier.updates = notifier.updates.filter((update) => !options.ignoreDistTags.includes(update.distTag));
 
     const header = chalk.bold(`${this.config.bin}-plugin update${notifier.updates.length > 1 ? 's' : ''} available!`);
-    if (!options.spawnOptions.detached) ux.action.stop();
     notifier.notify({ header, defer: options.defer });
   }
 };
